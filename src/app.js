189279558a812a58658861ddf7829c108aed7f93
kymchenet <149273045+kymchenet@users.noreply.github.com>
2024-08-16 17:51:20 +0000

    domainNameGenerator

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-/* eslint-disable */
+/* eslant-disable */
 import "bootstrap";
 import "./style.css";
 
@@ -6,20 +6,20 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  function dmGenerator() {
-    let pronoun = ["the", "our"];
-    let adj = ["great", "big"];
-    let noun = ["jogger", "racoon"];
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
 
-    let namesDomain = [];
+  let domainNames = [];
 
-    for (let one = 0; one < pronoun.length; one++) {
-      for (let two = 0; two < adj.length; two++) {
-        for (let three = 0; three < noun.length; three++) {
-          namesDomain.push(pronoun[one] + adj[two] + noun[three] + ".com");
-        }
+  for (let a = 0; a < pronoun.length; a++) {
+    for (let b = 0; b < adj.length; b++) {
+      for (let c = 0; c < noun.length; c++) {
+        let domain = pronoun[a] + adj[b] + noun[c] + ".com";
+        domainNames.push("[" + domain + "]");
       }
     }
   }
-  document.getElementById("#domainGenerator").innerHTML = dmGenerator();
+  let h1Element = document.getElementById("domain_name");
+  h1Element.innerHTML = domainNames.join("<br>");
 };
