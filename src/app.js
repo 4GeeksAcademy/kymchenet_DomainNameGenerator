cf458b2937c3c7e2558f1f6047b87b8712790fe4
kymchenet <149273045+kymchenet@users.noreply.github.com>
2024-08-16 16:54:25 +0000

    domainNameGnerator

--- src/app.js
+++ src/app.js
@@ -6,6 +6,20 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  function dmGenerator() {
+    let pronoun = ["the", "our"];
+    let adj = ["great", "big"];
+    let noun = ["jogger", "racoon"];
+
+    let namesDomain = [];
+
+    for (let one = 0; one < pronoun.length; one++) {
+      for (let two = 0; two < adj.length; two++) {
+        for (let three = 0; three < noun.length; three++) {
+          namesDomain.push(pronoun[one] + adj[two] + noun[three] + ".com");
+        }
+      }
+    }
+  }
+  document.getElementById("#domainGenerator").innerHTML = dmGenerator();
 };
